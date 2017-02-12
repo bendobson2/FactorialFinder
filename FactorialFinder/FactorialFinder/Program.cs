5637d3f58cdf649875f5b6d66698eb0a50a76490
Ben Dobson <Ben Dobson>
2017-02-11 23:55:13 -0500

    I made the program work using maths

--- FactorialFinder/FactorialFinder/Program.cs
+++ FactorialFinder/FactorialFinder/Program.cs
@@ -4,7 +4,30 @@ class FactorialFinder
 {
     static void Main()
     {
-        Console.WriteLine("I haven't finished this code yet.");
-        Console.ReadKey();
+        Console.WriteLine("Please input any integer value above one.");
+        int inputtedNum = Int32.Parse(Console.ReadLine()); //inputting the # 
+        while (inputtedNum < 1) //making sure the inputted # is valid.
+        {
+            Console.WriteLine("That number is invalid. Please input a valid number that is at least one.");
+            inputtedNum = Int32.Parse(Console.ReadLine());
+        }
+        string finalOutput = ("The factors for " + inputtedNum + " are ");//The string that will be outputted withall the factors
+        int counter = 2;
+        bool tvf = false;
+        while (counter < inputtedNum)
+        {
+            if ((inputtedNum % counter) == 0) //seeing if the number is a factor or not. 
+            {
+                finalOutput += (counter + " "); //adding the factor to the string
+                tvf = true;
+            }
+            counter += 1; //making the while loop work
+        }
+        if (tvf == true)
+        {
+            Console.WriteLine(finalOutput + ".");
+        }
+        else Console.WriteLine("The number you inputted is actually a prime number! Congrats! Wow!");
+        Console.ReadKey();//to make sure i can actually see the output. my cmd is dumb and closes too soon
     }
 }
